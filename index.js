9d6ae08e1eeb060c45962428bd8f749b767d0f47
SPeats <srpeters44@gmail.com>
2021-08-01 12:30:16 -0700

    added badges

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const questions = [
 {
   type: 'checkbox',
   message: "Please select tools used.",
-  choices: ["HTML5", "CSS3", "JavaScript", "Node.js", "jQuery", "Markdown", "Bootstrap", "UIKit", "Jest", "VSCode", "GitHub", "Heroku", "MySQL", "inquirer", "express", "sequelize", "Insomnia", "express-session", "express-handlebars", "bcrpyt", "dotenv", "MongoDB", "mongoose"],
+  choices: ["HTML5", "CSS3", "JavaScript", "Node.js", "jQuery", "Markdown", "Bootstrap", "UIKit", "Jest", "VSCode", "GitHub", "Heroku", "MySQL", "inquirer", "Express.js", "sequelize", "Insomnia", "express-session", "Handlebars", "bcrpyt", "dotenv", "MongoDB", "mongoose", "MongoDB Atlas", "JawsDB", "NPM", "Moment.js", "webpack", "IndexedDB"],
   name: 'tools',
 },
 {
